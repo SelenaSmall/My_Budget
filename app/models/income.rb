2b1571a95be81860ce9396b581e6ad3a5834ccc9
SelenaSmall <selenawiththetattoo@gmail.com>
2015-09-12 00:10:41 +1200

    Basic Css changes MyBudget Application

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,18 @@
 class Income < ActiveRecord::Base
-	belongs_to :welcome
+#	include ActiveModel::Model
+
+#	attr_accessor :wages, :other_income, :total_income
+
+#	def set_income_total
+#		@incomes.each do |income|
+#	a = incomes.wages
+#	b = incomes.other_income
+#	c = incomes.total_income
+#	end
+#	@set_total_income == (a+b)
+	
+#	end
+
 end
+
+	
